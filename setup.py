f8273a4410cd335260d458f785540f42a50925c8
Gwyn Uttmark <biosafetylevel5@gmail.com>
2024-07-11 12:10:07 -0600

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="pinkrst",
     version="0.0.1a",
     author="BSL5",
     description="Opinionated ReStructured Text (.rst) Formatter",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     entry_points={
         "console_scripts": ["pink=pinkrst.pinkrst:main"],
     },
-    install_requires=["argparse","rich"],
+    install_requires=["argparse", "rich"],
     python_requires=">3.5",
 )
